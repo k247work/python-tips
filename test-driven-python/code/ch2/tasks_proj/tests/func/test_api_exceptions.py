e9e993db9f1e7a75e1ff10c98686fda2f146ae17
k247work <k247work@gmail.com>
2021-04-07 20:24:19 +0900

    update api_exceptions add test class

--- test-driven-python/code/ch2/tasks_proj/tests/func/test_api_exceptions.py
+++ test-driven-python/code/ch2/tasks_proj/tests/func/test_api_exceptions.py
@@ -1,3 +1,4 @@
+import py
 import pytest
 import tasks
 
@@ -15,3 +16,16 @@ def test_start_tasks_db_raises():
         tasks.start_tasks_db('some/great/path', 'mysql')
     exception_msg = excinfo.value.args[0]
     assert exception_msg == "db_type must be a 'tiny' or 'mongo'"
+
+class TestUpdate():
+    """Test expected exceptions with tasks.update()"""
+
+    def test_bad_id(self):
+        """A non-int id should raise an exception."""
+        with pytest.raises(TypeError):
+            tasks.update(task_id={'dict instead':1}, task=tasks.Task())
+
+    def test_bad_task(self):
+        """A non-Task task should raise an exception."""
+        with pytest.raises(TypeError):
+            tasks.update(task_id=1, task='not a task')
