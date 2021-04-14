b1c14960b28318f086abae7f3c75555a428ea166
k247work <k247work@gmail.com>
2021-04-14 17:44:37 +0900

    add class TestAdd, def test_add_6

--- test-driven-python/code/ch2/tasks_proj/tests/func/test_add_variety.py
+++ test-driven-python/code/ch2/tasks_proj/tests/func/test_add_variety.py
@@ -61,6 +61,33 @@ def test_add_5(task):
     t_from_db = tasks.get(task_id)
     assert equivalent(t_from_db, task)
 
+@pytest.mark.parametrize('task', tasks_to_try, ids=task_ids)
+class TestAdd():
+    """Demonstrate parametrize and test calsses."""
+
+    def test_equivalent(self, task):
+        """Similar test, just within a class."""
+        task_id = tasks.add(task)
+        t_from_db = tasks.get(task_id)
+        assert equivalent(t_from_db, task)
+    
+    def test_valid_id(self, task):
+        """We can use the same data for multiple tests."""
+        task_id = tasks.add(task)
+        t_from_db = tasks.get(task_id)
+        assert t_from_db.id == task_id
+
+@pytest.mark.parametrize('task', [
+    pytest.param(Task('create'), id='just summary'),
+    pytest.param(Task('inspire', 'Michelle'), id='summary/owner'),
+    pytest.param(Task('encourage', 'Michelle', True),
+            id='summary/owner/done')
+])
+def test_add_6(task):
+    """Demonstrate pytest.param and id."""
+    task_id = tasks.add(task)
+    t_from_db = tasks.get(task_id)
+    assert equivalent(t_from_db, task)
 
 @pytest.fixture(autouse=True)
 def initialized_tasks_db(tmpdir):
