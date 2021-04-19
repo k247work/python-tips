d367458b2ba817c779da0e30f9faa55ab4f7a27a
k247work <k247work@gmail.com>
2021-04-19 21:07:19 +0900

    add ch3/ b .. conftest.py, test_autuse.py

---
+++ test-driven-python/code/ch3/b/tasks_proj/tests/conftest.py
@@ -0,0 +1,48 @@
+import pytest
+import tasks
+from tasks import Task
+
+@pytest.fixture(scope='session')
+def tasks_db_session(tmpdir_factory):
+    """Connect to db before tests, disconnect after."""
+    temp_dir = tmpdir_factory.mktemp('temp')
+    tasks.start_tasks_db(str(temp_dir), 'tiny')
+    yield
+    tasks.stop_tasks_db()
+
+@pytest.fixture()
+def tasks_db(tasks_db_session):
+    """An empty tasks db."""
+    tasks.delete_all()
+
+# about interface of Task constructor
+# Task(summary=None, owner=None, done=False, id=None)
+# summary: required
+# owner, done: option
+# id: defined by db
+
+@pytest.fixture(scope='session')
+def tasks_just_a_few():
+    """All summaries and owners are unique."""
+    return(
+        Task('Write some code', 'Brian', True),
+        Task("Code review Brian's code", 'Katie', False),
+        Task('Fix what Brian did', 'Michelle', False)
+    )
+
+@pytest.fixture(scope='session')
+def tasks_mult_per_owner():
+    """Several owners with several tasks each."""
+    return(
+        Task('Make a cookie', 'Raphael'),
+        Task("Use an emoji", 'Raphael'),
+        Task('Move to Berlin', 'Raphael'),
+
+        Task('Create', 'Michelle'),
+        Task('Inspire', 'Michelle'),
+        Task('Encourage', 'Michelle'),
+        
+        Task('Do a handstand', 'Daniel'),
+        Task('Write some books', 'Daniel'),
+        Task('Eat ice cream', 'Daniel')
+    )
