2aa4d15cf7d4c0d00803c5a28c36d6f4854adf3d
k247work <k247work@gmail.com>
2021-05-06 18:02:16 +0900

    add monkeypatch expanduser

--- test-driven-python/code/ch4/monkey/test_cheese.py
+++ test-driven-python/code/ch4/monkey/test_cheese.py
@@ -13,3 +13,12 @@ def test_def_prefs_change_home(tmpdir, monkeypatch):
     expected = cheese._default_prefs
     actual = cheese.read_cheese_preferences()
     assert expected == actual
+    
+def test_def_prefs_change_expanduser(tmpdir, monkeypatch):
+    fake_home_dir = tmpdir.mkdir('home')
+    monkeypatch.setattr(cheese.os.path, 'expanduser', 
+                (lambda x: x.replace('~', str(fake_home_dir))))
+    cheese.write_default_cheese_preferences()
+    expected = cheese._default_prefs
+    actual = cheese.read_cheese_preferences()
+    assert expected == actual
