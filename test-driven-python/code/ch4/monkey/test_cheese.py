d689e74a900a65fa09f39a732e254f430c6cc30d
k247work <k247work@gmail.com>
2021-05-07 08:30:18 +0900

    add monkey change default

--- test-driven-python/code/ch4/monkey/test_cheese.py
+++ test-driven-python/code/ch4/monkey/test_cheese.py
@@ -1,5 +1,6 @@
 import pytest
 import cheese
+import copy
 
 def test_def_prefs_full():
     cheese.write_default_cheese_preferences()
@@ -22,3 +23,25 @@ def test_def_prefs_change_expanduser(tmpdir, monkeypatch):
     expected = cheese._default_prefs
     actual = cheese.read_cheese_preferences()
     assert expected == actual
+
+def test_def_prefs_change_defaults(tmpdir, monkeypatch):
+    # write file at once
+    fake_home_dir = tmpdir.mkdir('home')
+    monkeypatch.setattr(cheese.os.path, 'expanduser',
+            (lambda x: x.replace('~', str(fake_home_dir))))
+    cheese.write_default_cheese_preferences()
+    defaults_before = copy.deepcopy(cheese._default_prefs)
+
+    # change default values
+    monkeypatch.setitem(cheese._default_prefs, 'slicing', ['provolone'])
+    monkeypatch.setitem(cheese._default_prefs, 'spreadable', ['brie'])
+    monkeypatch.setitem(cheese._default_prefs, 'salads', ['pepper jack'])
+    defaults_modified = cheese._default_prefs
+
+    # re-output with changed default
+    cheese.write_default_cheese_preferences()
+
+    # read file and check
+    actual = cheese.read_cheese_preferences()
+    assert defaults_modified == actual
+    assert defaults_modified != defaults_before
