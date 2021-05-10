4150d96810b42111a3b467e94be92d4f6a1710f1
k247work <k247work@gmail.com>
2021-05-10 19:09:06 +0900

    add test_warnings

---
+++ test-driven-python/code/ch4/test_warnings.py
@@ -0,0 +1,12 @@
+import warnings
+import pytest
+
+def lame_function():
+    warnings.warn("Please stop using this", DeprecationWarning)
+
+def test_lame_function(recwarn):
+    lame_function()
+    assert len(recwarn) == 1
+    w = recwarn.pop()
+    assert w.category == DeprecationWarning
+    assert str(w.message) == 'Please stop using this'
