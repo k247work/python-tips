b5b1f85e3748d8ba7df4c971041f272cef7304df
k247work <k247work@gmail.com>
2021-05-12 08:29:19 +0900

    ch5 herb07

---
+++ test-driven-python/code/ch5/a/tasks_proj/tests/func/test_unique_id_1.py
@@ -0,0 +1,19 @@
+import pytest
+import tasks
+
+def test_unique_id():
+    """Calling unique_id twice should return different numbers."""
+    id_1 = tasks.unique_id()
+    id_2 = tasks.unique_id()
+    assert id_1 != id_2
+
+@pytest.fixture(autouse=True)
+def initialized_tasks_db(tmpdir):
+    """Connect to db before testing, disconnect after."""
+    # setup: start db connection
+    tasks.start_tasks_db(str(tmpdir), 'tiny')
+
+    yield # execute test
+
+    # teardown: terminate db connection
+    tasks.stop_tasks_db()
