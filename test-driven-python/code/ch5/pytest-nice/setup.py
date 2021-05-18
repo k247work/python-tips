2613676f788661267d1457a7c94c524b5f4994eb
k247work <k247work@gmail.com>
2021-05-18 17:37:55 +0900

    add setup and readme to pytest-nice

---
+++ test-driven-python/code/ch5/pytest-nice/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='pytest-nice',
+    version='0.1.0',
+    description='A pytest plugin to turn Failure into Opportunity',
+    url='https://<place this package introduced>',
+    author='<your name>',
+    author_email='<xxx@yyyy>',
+    license='proprietrary',
+    py_modules=['pytest'],
+    install_requires=['pytest'],
+    entry_points={'pytest1 1':['nuce = pytest_nice',],},
+)
