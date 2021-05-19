1d99383f145f0da2f7431ef89b3aae908b045658
k247work <k247work@gmail.com>
2021-05-20 07:59:44 +0900

    add test for plugin

---
+++ test-driven-python/code/ch5/pytest-nice/tests/test_nice.py
@@ -0,0 +1,20 @@
+def test_pass_fail(testdir):
+
+    # create temp pytest module
+    testdir.makepyfile("""
+        def test_pass():
+            assert 1 == 1
+        def test_fail():
+            assert 1 == 2
+    """)
+
+    # execute pytest
+    result = testdir.runpytest()
+
+    # fnmatch_lines execute assertion internally
+    result.stdout.fnmatch_lines([
+        '*.F*', # . means success, F means failure
+    ])
+
+    # check terminal code of test suite is 1
+    assert result.ret == 1
