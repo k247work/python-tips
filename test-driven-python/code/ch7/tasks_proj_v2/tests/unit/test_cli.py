f915c96cedfa1b26f797c6d5cd76784c944a7333
k247work <k247work@gmail.com>
2021-06-07 08:42:39 +0900

    add import to test_cli.py

--- test-driven-python/code/ch7/tasks_proj_v2/tests/unit/test_cli.py
+++ test-driven-python/code/ch7/tasks_proj_v2/tests/unit/test_cli.py
@@ -1,3 +1,9 @@
+from click.testing import CLiRunner
+from contextlib import contextmanager
+import pytest
+from tasks.api import Task
+import tasks.cli
+import tasks.config
 
 @contextmanager
 def stub_tasks_db():
