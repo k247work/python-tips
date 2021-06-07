9a66c51e56707a07789639f1b748ea6184520271
k247work <k247work@gmail.com>
2021-06-08 08:55:26 +0900

    add test_list_no_args

--- test-driven-python/code/ch7/tasks_proj_v2/tests/unit/test_cli.py
+++ test-driven-python/code/ch7/tasks_proj_v2/tests/unit/test_cli.py
@@ -8,3 +8,10 @@ import tasks.config
 @contextmanager
 def stub_tasks_db():
     yield
+
+def test_list_no_args(mocker):
+    mocker.patch.object(tasks.cli, '_tasks_db', new=stub_tasks_db)
+    mocker.patch.object(tasks.cli.tasks, 'list_tasks', return_value=[])
+    runner = CliRunner()
+    runner.invoke(tasks.cli.tasks_cli, ['list'])
+    tasks.cli.tasks.list_tasks.assert_called_onece_with(None)
